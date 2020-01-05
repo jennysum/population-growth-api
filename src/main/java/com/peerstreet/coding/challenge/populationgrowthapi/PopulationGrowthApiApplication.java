d93916040c44e2a460e32260e4467dcd2f13e852
Jenny Sum <jennysum@Jennys-MacBook-Air.local>
2020-01-05 00:12:41 -0600

    add project for the first time; add database properties

---
+++ src/main/java/com/peerstreet/coding/challenge/populationgrowthapi/PopulationGrowthApiApplication.java
@@ -0,0 +1,13 @@
+package com.peerstreet.coding.challenge.populationgrowthapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopulationGrowthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopulationGrowthApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peerstreet/coding/challenge/populationgrowthapi/PopulationGrowthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peerstreet.coding.challenge.populationgrowthapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopulationGrowthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
